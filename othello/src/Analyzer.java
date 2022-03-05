58b3d3a218e75a23c06e949816866704510d6bcb
AntoineJonard <antoine.jonard2@gmail.com>
2022-03-05 14:04:35 +0100

    plus de stats

--- othello/src/Analyzer.java
+++ othello/src/Analyzer.java
@@ -1,3 +1,4 @@
+import ia.AbosuluIA;
 import ia.IaBuilder;
 import ia.Type;
 import othello.Frame;
@@ -12,29 +13,38 @@ public class Analyzer {
 	private int nbRedWins;
 	private int nbBlackWins;
 	
-	private Side[][] wins;
+	private Side[][][] winsEqualDepth;
+	
+	private int[][] iaTypeWins;
 	
 	private int[][] fDepth;
+	
+	private int nbGames;
 
 	public Analyzer(int untilDepth) {
 		super();
 		this.untilDepth = untilDepth;
 		this.nbRedWins = 0;
+		nbGames = 0;
 		this.nbBlackWins = 0;
-		this.fDepth = new int[Type.values().length][untilDepth];
-		this.wins = new Side[Type.values().length][Type.values().length];
+		this.fDepth = new int[Type.values().length][untilDepth+1];
+		this.winsEqualDepth = new Side[untilDepth+1][Type.values().length][Type.values().length];
+		iaTypeWins = new int [Type.values().length][1];
 	}
 
 	public void analyze() {
 		
 		for (Type p1 : Type.values()) {
 			for (Type p2 : Type.values()) {
-				for (int depthP1 = 0; depthP1 < untilDepth ; depthP1++) {
-					for (int depthP2 = 0; depthP2 < untilDepth ; depthP2++) {
+				for (int depthP1 = 0; depthP1 <= untilDepth ; depthP1++) {
+					for (int depthP2 = 0; depthP2 <= untilDepth ; depthP2++) {
 						Player black = IaBuilder.getIA(p1, depthP1);
 						Player red = IaBuilder.getIA(p2, depthP2);
 						
 						Game game = new Game();
+						
+						black.setGame(game, Side.BLACK);
+						red.setGame(game, Side.RED);
 						
 						Side winner = null;
 						
@@ -44,17 +54,22 @@ public class Analyzer {
 							System.out.println("Memory limitation for depth :" + depthP1 + "(black)" + depthP2 + "(red)");
 						}
 						
-						if (winner == Side.RED)
+						if (winner == Side.RED) {
 							nbRedWins++;
-						else 
+							fDepth[p2.ordinal()][depthP2]++;
+							iaTypeWins[p2.ordinal()][0]++;
+						}
+						else {
+							iaTypeWins[p1.ordinal()][0]++;
 							nbBlackWins++;
-						
+							fDepth[p1.ordinal()][depthP1]++;
+						}
+
 						if (depthP1 == depthP2) {
-							wins[p1.ordinal()][p2.ordinal()] = winner;
+							winsEqualDepth[depthP1][p1.ordinal()][p2.ordinal()] = winner;
 						}
 						
-						fDepth[p1.ordinal()][depthP1]++;
-						fDepth[p2.ordinal()][depthP2]++;
+						nbGames++;
 					}	
 				}
 			}	
@@ -79,11 +94,85 @@ public class Analyzer {
 	}
 	
 	public void displayResult() {
-		System.out.println("Red wins : "+nbRedWins+"\nBlack wins :"+nbBlackWins);
+		System.out.println("Results");
+		System.out.println("_________\n");
+		System.out.println("Red wins :\t"+nbRedWins+"\nBlack wins :\t"+nbBlackWins);
+		System.out.println("_________\n");
+		displayWinsEqualDepth();
+		System.out.println("_________\n");
+		displayWinsAccordingToDepth();
+		System.out.println("_________\n");
+		displayIaWinsGraphe();
+	
+	}
+
+	private void displayIaWinsGraphe() {
+		System.out.println("Wins percent\n");
 		
 		for (Type type : Type.values()) {
-			System.out.print(type+"\t");
+			System.out.print(fixedLengthString(type.toString(), 15));
+			float winPercent = ((float)iaTypeWins[type.ordinal()][0])/nbGames*100;
+			System.out.print("|");
+			for (int i = 0 ; i < winPercent ; i++) {
+				System.out.print("-");
+			}
+			System.out.println("> "+winPercent+"%");
 		}
+	}
+
+	private void displayWinsAccordingToDepth() {
+		System.out.println("Wins depending on Depth\n");
+		
+		System.out.print(fixedLengthString("IA type \\ Depth", 20));
+		
+		for (int depth = 0 ; depth <= untilDepth ; depth++) {
+			System.out.print(fixedLengthString(Integer.toString(depth, 20),20));
+		}
+		
+		System.out.println();
+		
+		for (Type type : Type.values()) {
+			
+			System.out.print(fixedLengthString(type.toString(), 20));
+			
+			for (int depth = 0 ; depth <= untilDepth ; depth++) {
+				System.out.print(fixedLengthString(Integer.toString(fDepth[type.ordinal()][depth]), 20));
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+	private void displayWinsEqualDepth() {
+		
+		for (int depth = 0 ; depth <= untilDepth ; depth++) {
+			System.out.println("Wins with equal depth for BLACK and RED : "+depth+"\n");
+			
+			System.out.print(fixedLengthString("BLACK \\ RED", 15));
+			
+			for (Type type : Type.values()) {
+				System.out.print(fixedLengthString(type.toString(), 15));
+			}
+			
+			System.out.println();
+			
+			for (Type type : Type.values()) {
+				
+				System.out.print(fixedLengthString(type.toString(), 15));
+				
+				for (Type typeAgainst : Type.values()) {
+					System.out.print(fixedLengthString(winsEqualDepth[depth][type.ordinal()][typeAgainst.ordinal()].toString(), 15));
+				}
+				
+				System.out.println();
+			}
+			System.out.println();
+		}
+
+	}
 	
+	private static String fixedLengthString(String string, int length) {
+	    return String.format("%-" + length + "." + length + "s", string);
 	}
 }
---
+++ othello/src/TestAnalyzer.java
@@ -0,0 +1,12 @@
+
+public class TestAnalyzer {
+
+	public static void main(String[] args) {
+		
+		Analyzer analyzer = new Analyzer(4);
+		analyzer.analyze();
+		analyzer.displayResult();
+
+	}
+
+}
